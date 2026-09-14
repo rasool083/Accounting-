a015d27c4e4eb7360141d0e1b4d6580e989cb81f
rasool083 <rghoddoosi@gmail.com>
2026-09-14 07:08:26 +0330

    feat: load UI and discount integration patches

--- ui-final-v4.js
+++ ui-final-v4.js
@@ -1,2 +1,2 @@
 'use strict';
-(function(){var s=document.createElement('script');s.src='ui-phase05.js?v=20260914-ui8';document.currentScript.parentNode.insertBefore(s,document.currentScript.nextSibling);})();
+(function(){var s=document.createElement('script');s.src='ui-phase05.js?v=20260914-ui8';s.onload=function(){var p=document.createElement('script');p.src='ui-phase05-patch.js?v=20260914-ui8';document.currentScript.parentNode.insertBefore(p,document.currentScript.nextSibling)};document.currentScript.parentNode.insertBefore(s,document.currentScript.nextSibling);})();
